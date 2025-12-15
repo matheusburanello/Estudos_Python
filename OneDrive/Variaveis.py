b0b9645e6fdaf88a9b231e630c9855b6d6b81022
Matheus Rocha <matheusburanello21@icloud.com>
2025-12-14 21:49:58 -0300

    Estudo sobre variaveis

---
+++ OneDrive/Variaveis.py
@@ -0,0 +1,33 @@
+nome = "Matheus"
+sobrenome = "Buranello"
+idade = 24
+ano_de_nascimento = 2000
+altura = 1.75
+print("\n")
+
+print("Nome: ", nome)
+print("Sobrenome:", sobrenome)
+print("Idade:", idade)
+print("Ano de nascimento: ", ano_de_nascimento)
+print("Altura: ", altura)
+
+print("\n")
+
+a1 = 2000
+a2 = 24
+print("A soma dos valores é o ano de:", a1 + a2)
+
+print("\n")
+
+nome2 = "Pietra Silveira Turato"
+idade2 = 19
+maior_de_idade = idade >= 18
+fim_do_programa = "--Sair do sistema"
+
+# este operador -> >= quer dizer MAIOR ou IGUAL
+
+print("")
+print("Nome completo: ", nome2, "-", "Idade:", idade2)
+print("Ela é maior de idade?", maior_de_idade)
+print("") # Este print pula uma linha, o mesmo retorno do print ('\n')
+print(fim_do_programa)
