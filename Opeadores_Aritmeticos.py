86b5efcad6fc56ec2021a8e92d20303dbbfdbddf
Matheus Rocha <matheusburanello21@icloud.com>
2025-12-14 21:53:30 -0300

    Operações Aritméticas

---
+++ Opeadores_Aritmeticos.py
@@ -0,0 +1,39 @@
+
+# Operador Soma
+adicao = 2000 + 24
+print("O ano que estampos é:", adicao)
+print("\n")
+
+# Operador de Subtração
+subtracao = 2024 - 1
+print("O ano que se passou foi: ", subtracao)
+print("\n")
+
+# Operador de Multiplicação
+qtd_mem = "GB"
+multiplicacao = 2 * 2048
+print("Tenho dois pentes de memoria de 2GB, então tenho essa quantidade de memoria RAM: ", multiplicacao, qtd_mem)
+print("\n")
+
+# Outro exemplo de multiplicação mas com variaveis:
+qtd_caracter = "F"
+calculo = 10 * qtd_caracter
+msg = "O resultado da multiplicação de String é: "
+print(msg,calculo)
+print("\n")
+
+# Operador de Exponenciação
+exponeciacao = 2 ** 4
+print("A quantidade maxima de memoria que minha placa-mae suporta é de: ",exponeciacao,end="GB")
+print("\n")
+
+# Operador --> % de RESTO da divisão
+resto_divisao = 26 % 4
+print("O resto da divisão informada é de: ",resto_divisao)
+print("\n")
+
+# Operador de Divisão "inteira"
+notacao_valor = "R$"
+divisao_inteira = 30 // 10.50
+print("O troco a devolver ao cliente é: ", notacao_valor,divisao_inteira,end="0")
+print("\n")
