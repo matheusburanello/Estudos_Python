28abe8b122d941b0638835494ac8658c908cfb77
Matheus Rocha <matheusburanello21@icloud.com>
2025-12-26 14:35:49 -0300

    concatenação

---
+++ concatenação.py
@@ -0,0 +1,19 @@
+# Concatenção tem como finalidae de JUNTAR valores
+
+concatenar = "M" + "B" + "R"
+siglas = "A sigla do meu nome é:"
+nc = "e meu nome completo é"
+nome = "Matheus"
+nome_do_meio = "Buranello"
+sobrenome = "Rocha"
+print("\n")
+print(siglas, concatenar, nc, nome, nome_do_meio, sobrenome)
+
+print("\n")
+concatenar2 = "F" + "L" + "A"
+dez_vezes_F = "F" * 10
+msg = "Veja como é importante concatenar..."
+print("\n")
+print(msg)
+print("\n")
+print(dez_vezes_F, concatenar2) # Imprimindo dois valores no mesmo print
